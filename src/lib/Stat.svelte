3206cb482513ba9531042aa1a565ae4966d030cf
Christoph <christoph.thonhauser@squer.at>
2022-04-11 17:20:19 +0200

    change temp

--- src/lib/Stat.svelte
+++ src/lib/Stat.svelte
@@ -2,7 +2,7 @@
 
   <div class="stat">
     <div class="stat-title">Temperature</div>
-    <div class="stat-value">23 Celsius</div>
+    <div class="stat-value">25 Celsius</div>
     <div class="stat-desc">Lowest: 17C</div>
     <div class="stat-desc">Highest: 25C</div>
   </div>
