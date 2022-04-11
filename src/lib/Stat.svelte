515d15ced443b53dd74f5c860057652cf87e839f
Christoph <christoph.thonhauser@squer.at>
2022-04-11 17:18:34 +0200

    display current weather

--- src/lib/Stat.svelte
+++ src/lib/Stat.svelte
@@ -1,9 +1,10 @@
 <div class="stats shadow">
 
   <div class="stat">
-    <div class="stat-title">Total Page Views</div>
-    <div class="stat-value">89,400</div>
-    <div class="stat-desc">21% more than last month</div>
+    <div class="stat-title">Temperature</div>
+    <div class="stat-value">23 Celsius</div>
+    <div class="stat-desc">Lowest: 17C</div>
+    <div class="stat-desc">Highest: 25C</div>
   </div>
 
 </div>
