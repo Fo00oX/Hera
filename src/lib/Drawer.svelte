41f3c1c9b80efa70eae9ecb4869f61b314c84525
Christoph <christoph.thonhauser@squer.at>
2022-04-26 21:02:41 +0200

    use custom fonts and semantic elements

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -8,9 +8,9 @@
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current text-white"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
       </div>
-      <div class='flex-1'>
+      <h1 class='flex-1'>
         <a href='./' class='btn btn-ghost normal-case text-xl text-white'>Wiener Wölkchen</a>
-      </div>
+      </h1>
       <div class="flex-none hidden lg:block text-white">
         <ul class="menu menu-horizontal">
           <!-- Navbar menu content here -->
