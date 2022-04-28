493e6610e94a2964b7bcb4cf0f357d58965e73b9
Christoph <christoph.thonhauser@squer.at>
2022-04-28 10:21:12 +0200

    add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script lang='ts'>
   import Drawer from './lib/Drawer.svelte';
+  import Footer from './lib/Footer.svelte';
 </script>
 
 <Drawer>
-  <main class='p-6'>
-    Content
-  </main>
+  <div class='flex flex-col justify-between h-full'>
+    <main class='p-6'>
+      Content
+    </main>
+  </div>
+  <Footer />
 </Drawer>
---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class='footer items-center p-4 bg-neutral text-neutral-content flex space-x-8'>
+  <div class='items-center grid-flow-col'>
+    <div class='avatar'>
+      <div class='w-12 rounded-xl'>
+        <img src='logo/cumulus.jpeg' alt='logo'>
+      </div>
+    </div>
+    <p>Wiener Wölkchen © 2022 <br> All right reserved</p>
+  </div>
+  <div class='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
+    <a href='https://github.com/Fo00oX/Hera'><img src='github/GitHub-Mark-Light-32px.png' alt='github-logo'></a>
+  </div>
+</footer>
