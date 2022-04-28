d4f839985530ce4bbc62fb4ea545519e554cab6f
Christoph <christoph.thonhauser@squer.at>
2022-04-28 11:09:33 +0200

    add current weather

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 <script lang='ts'>
   import Drawer from './lib/Drawer.svelte';
   import Footer from './lib/Footer.svelte';
+  import CurrentWeatherCard from './lib/CurrentWeatherCard.svelte';
 </script>
 
 <Drawer>
   <div class='flex flex-col justify-between h-full'>
     <main class='p-6'>
-      Content
+      <CurrentWeatherCard></CurrentWeatherCard>
     </main>
   </div>
   <Footer />
---
+++ src/lib/CurrentWeatherCard.svelte
@@ -0,0 +1,25 @@
+<script lang='ts' context='module'>
+  function inCelsius(temp: number) {
+    return temp + '\xB0C';
+  }
+</script>
+
+<div class='stats shadow w-full max-w-lg'>
+
+  <div class='stat flex justify-around items-center'>
+    <div>
+      <div class='stat-title font-bold'>Aktuell</div>
+      <div class='stat-value'>{inCelsius(25)}</div>
+      <div class='stat-desc text-primary'>Min: {inCelsius(17)}</div>
+      <div class='stat-desc text-warning'>Max: {inCelsius(27)}</div>
+    </div>
+    <div class='stat-figure text-secondary'>
+      <div class='avatar'>
+        <div class='w-32 rounded-full'>
+          <img src='weather/clear-day.svg' />
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
