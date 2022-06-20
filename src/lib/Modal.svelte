5916b5c9092a25a24381e3ad5f5971e53d3143b2
Christoph <christoph.thonhauser@squer.at>
2022-06-20 21:39:31 +0200

    add generic modal

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+  import { writable } from 'svelte/store';
+  import Register from '../routes/auth/Register.svelte';
+  import Modal from 'svelte-simple-modal';
+
+  const modal = writable(null);
+
+  export const showModal = () => modal.set(Register);
+</script>
+
+<Modal show={$modal}>
+  <button class='btn btn-ghost rounded-lg normal-case' on:click={showModal}>Register</button>
+</Modal>
